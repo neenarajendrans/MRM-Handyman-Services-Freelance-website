3e12f51322d89b472783116df2baa6943c550613
Neena <neenarajendran1999@gmail.com>
2025-10-28 21:45:37 +0530

    Form submission using formspree

--- script.js
+++ script.js
@@ -38,9 +38,37 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
   // Form submission
-        document.querySelector('.contact-form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Thank you for your message! We will contact you soon.');
-            this.reset();
-           
-        });
+ var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        status.style.color = "green"; 
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            status.style.color = "red";
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+            status.style.color = "red";
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+      status.style.color = "red";
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
